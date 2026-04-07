aa000de03d7bf0b275455af369b3c47650236d0a
nirmitprasad <nirmitprasad@gmail.com>
2026-04-08 00:21:11 +0530

    Backend Update

--- scan_sidebar.py
+++
@@ -1,6 +0,0 @@
-lines=open('frontend/app.py',encoding='utf-8',errors='replace').readlines()
-with open('sidebar_locs.txt','w') as f:
-    for i,l in enumerate(lines):
-        if 'st.sidebar' in l or 'with st.sidebar' in l:
-            f.write(f"{i+1}|{l.rstrip()[:100]}\n")
-print("done")
